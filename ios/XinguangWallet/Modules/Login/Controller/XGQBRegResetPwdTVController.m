8cb1f1cdee5b91fd1daeb1664449d25bea8dffd4
JIN Ke <zjujoke@qq.com>
2017-10-24 16:54:25 +0800

    首页登录注册接口优化

--- ios/XinguangWallet/Modules/Login/Controller/XGQBRegResetPwdTVController.m
+++ ios/XinguangWallet/Modules/Login/Controller/XGQBRegResetPwdTVController.m
@@ -13,10 +13,17 @@
 
 @interface XGQBRegResetPwdTVController ()
 
+@property (nonatomic,strong) XGQBTextField *phoneNoTextField;
+@property (nonatomic,strong) XGQBTextField *regCodeTextField;
+@property (nonatomic,strong) XGQBTextField *payPwdTextField;
+@property (nonatomic,strong) XGQBTextField *loginPwdTextField;
+@property (nonatomic,strong) XGQBTextField *idNoTextField;
+
 @end
 
 @implementation XGQBRegResetPwdTVController
 
+#pragma mark - 工厂方法
 +(instancetype)tableVCWithType:(XGQBRegResetPwdTVConType)type
 {
     XGQBRegResetPwdTVController *tVC = [XGQBRegResetPwdTVController new];
@@ -27,16 +34,25 @@
     return tVC;
 }
 
+- (instancetype)initWithStyle:(UITableViewStyle)style
+{
+    return [super initWithStyle:UITableViewStylePlain];
+}
+
+
+#pragma mark - VC生命周期
 - (void)viewDidLoad {
     
     [super viewDidLoad];
     
+    NSString *btnName = [NSString stringWithFormat:@"确认"];
     
     self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
     
     switch (self.type) {
         case XGQBRegResetPwdTVConTypeRegister:
             self.title = @"注册";
+            btnName = @"注册";
             break;
         case XGQBRegResetPwdTVConTypeResetLoginPwd:
             self.title = @"重置登录密码";
@@ -53,7 +69,7 @@
     
     UIView *footerView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, kScreenWidth, 200)];
     
-    XGQBPureColorBtn *btn = [XGQBPureColorBtn buttonWithText:@"确认" andColor:[UIColor colorWithHexString:@"F34646"]];
+    XGQBPureColorBtn *btn = [XGQBPureColorBtn buttonWithText:btnName andColor:[UIColor colorWithHexString:@"F34646"]];
     [footerView addSubview:btn];
     
     [btn mas_makeConstraints:^(MASConstraintMaker *make) {
@@ -62,31 +78,138 @@
         make.top.equalTo(footerView).with.offset(50);
     }];
     
+    [btn addTarget:self action:@selector(comfirmBtnClicked:) forControlEvents:UIControlEventTouchUpInside];
+    
     self.tableView.tableFooterView = footerView;
     
 }
-
 
 -(void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
     self.navigationController.navigationBarHidden = NO;
+    [UIApplication sharedApplication].statusBarStyle = UIStatusBarStyleDefault;
 }
 
-- (instancetype)initWithStyle:(UITableViewStyle)style
+
+#pragma mark - 处理按钮点击
+-(void)comfirmBtnClicked:(XGQBPureColorBtn*)btn
 {
-    return [super initWithStyle:UITableViewStylePlain];
+    [self.view endEditing:YES];
+    
+    //判断手机号跟密码格式是否正确
+    if([_phoneNoTextField.text isEqualToString:@""])
+    {
+        [SVProgressHUD showInfoWithStatus:@"手机号不能为空"];
+        return;
+    }else if (![_phoneNoTextField.text checkMobile])
+    {
+        [SVProgressHUD showInfoWithStatus:@"请输入正确手机号"];
+        return;
+    }
+    
+    if([_loginPwdTextField.text isEqualToString:@""])
+    {
+        [SVProgressHUD showInfoWithStatus:@"密码不能为空"];
+        return;
+    }else if (![_loginPwdTextField.text checkPassword])
+    {
+        [SVProgressHUD showInfoWithStatus:@"请输入6至20位字母加数字"];
+        return;
+    }
+    
+    NSMutableDictionary* body = [NSMutableDictionary dictionaryWithCapacity:0];
+    if (_phoneNoTextField.text) {
+        [body setObject:_phoneNoTextField.text forKey:@"phone"];
+    }
+    if (_regCodeTextField.text) {
+        [body setObject:_regCodeTextField.text forKey:@"smsCode"];
+    }
+    if (_loginPwdTextField.text) {
+        [body setObject:_loginPwdTextField.text forKey:@"password"];
+    }
+    if (_payPwdTextField.text) {
+        [body setObject:_payPwdTextField.text forKey:@"payPassword"];
+    }
+    if (self.type == XGQBRegResetPwdTVConTypeRegister) {
+        //发送注册请求
+        [MemberCoreService registerUser:body andSuccessFn:^(id responseAfter, id responseBefore) {
+            NSLog(@"successWithRetCode:%d",[[responseBefore objectForKey:@"retCode"] intValue]);
+            [responseBefore writeToFile:@"/Users/bossking/Desktop/responseBefore.plist" atomically:YES];
+            if([[responseBefore objectForKey:@"retCode"] intValue] == 1)
+                //            NSLog(@"responseBefore:%@",responseBefore);
+            {
+                [SVProgressHUD showSuccessWithStatus:@"注册成功"];
+            }
+        } andFailerFn:^(NSError *error) {
+            
+        }];
+    }
+    else if(self.type == XGQBRegResetPwdTVConTypeResetLoginPwd){
+        [MemberCoreService resetLoginPassword:body andSuccessFn:^(id responseAfter, id responseBefore) {
+            if([[responseBefore objectForKey:@"retCode"] intValue] == 1)
+                //            NSLog(@"responseBefore:%@",responseBefore);
+            {
+                [SVProgressHUD showSuccessWithStatus:@"重置密码成功"];
+            }
+        } andFailerFn:^(NSError *error) {
+            
+        }];
+    }
+    
+
 }
 
+-(void)countTimeBtnClicked:(XGQBCountTimeBtn*)btn
+{
+    //判断手机号跟密码格式是否正确
+    if([_phoneNoTextField.text isEqualToString:@""])
+    {
+        [SVProgressHUD showInfoWithStatus:@"手机号不能为空"];
+        return;
+    }else if (![_phoneNoTextField.text checkMobile])
+    {
+        [SVProgressHUD showInfoWithStatus:@"请输入正确手机号"];
+        return;
+    }
+    
+    NSMutableDictionary* body = [NSMutableDictionary dictionaryWithCapacity:0];
+    [body setObject:_phoneNoTextField.text forKey:@"phone"];
+    
+    //注册页面获取验证码
+    if (self.type == XGQBRegResetPwdTVConTypeRegister) {
+        [MemberCoreService sendRegisterCode:body andSuccessFn:^(id responseAfter, id responseBefore) {
+            NSLog(@"successWithRetCode:%d",[[responseBefore objectForKey:@"retCode"] intValue]);
+            [responseBefore writeToFile:@"/Users/bossking/Desktop/responseBefore.plist" atomically:YES];
+            if([[responseBefore objectForKey:@"retCode"] intValue] == 1)
+            {
+                [SVProgressHUD showSuccessWithStatus:@"验证码发送成功"];
+                [btn startCountDown];
+            }
+        } andFailerFn:^(NSError *error) {
+            
+        }];
+    }
+    //重置登录密码页面获取验证码
+    else if(self.type == XGQBRegResetPwdTVConTypeResetLoginPwd){
+        [MemberCoreService sendResetLoginPasswordCode:body andSuccessFn:^(id responseAfter, id responseBefore) {
+            NSLog(@"successWithRetCode:%d",[[responseBefore objectForKey:@"retCode"] intValue]);
+            [responseBefore writeToFile:@"/Users/bossking/Desktop/responseBefore.plist" atomically:YES];
+            if([[responseBefore objectForKey:@"retCode"] intValue] == 1)
+            {
+                [SVProgressHUD showSuccessWithStatus:@"验证码发送成功"];
+                [btn startCountDown];
+            }
+        } andFailerFn:^(NSError *error) {
+            
+        }];
+    }
+    
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
 }
 
 #pragma mark - Table view data source
-
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     return 1;
 }
@@ -105,35 +228,40 @@
         default:
             return 0;
     }
-
 }
-
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     
     if (indexPath.section==0&&indexPath.row==0) {
         //第一行手机号
       XGQBRegRestPwdTVCell*cell = [XGQBRegRestPwdTVCell cellWithType:XGQBRegResetPwdTVCellTypePhoneNo];
+        _phoneNoTextField = cell.textField;
         return cell;
     }else if (indexPath.section==0&&indexPath.row==1) {
         //第二行验证码
        XGQBRegRestPwdTVCell *cell = [XGQBRegRestPwdTVCell cellWithType:XGQBRegResetPwdTVCellTypeRegCode];
+        [cell.countTimeBtn addTarget:self action:@selector(countTimeBtnClicked:) forControlEvents:UIControlEventTouchUpInside];
+        _regCodeTextField = cell.textField;
         return cell;
     }else if (indexPath.section==0&&indexPath.row==2) {
         //第三行
         if (self.type == XGQBRegResetPwdTVConTypeRegister) {
             XGQBRegRestPwdTVCell *cell = [XGQBRegRestPwdTVCell cellWithType:XGQBRegResetPwdTVCellTypeLoginPwd];
+            _loginPwdTextField = cell.textField;
             return cell;
         }else if(self.type ==XGQBRegResetPwdTVConTypeResetLoginPwd){
             XGQBRegRestPwdTVCell *cell = [XGQBRegRestPwdTVCell cellWithType:XGQBRegResetPwdTVCellTypeLoginPwd];
+            _loginPwdTextField = cell.textField;
             return cell;
         }else if (self.type ==XGQBRegResetPwdTVConTypeResetPayPwdWithID)
         {
             XGQBRegRestPwdTVCell *cell = [XGQBRegRestPwdTVCell cellWithType:XGQBRegResetPwdTVCellTypeIDNo];
+            _idNoTextField = cell.textField;
             return cell;
         }else if (self.type ==XGQBRegResetPwdTVConTypeResetPayPwdNoID)
         {
             XGQBRegRestPwdTVCell *cell = [XGQBRegRestPwdTVCell cellWithType:XGQBRegResetPwdTVCellTypePayPwd];
+            _payPwdTextField = cell.textField;
             return cell;
         }else{
             return nil;
@@ -142,10 +270,12 @@
         //第四行
         if (self.type == XGQBRegResetPwdTVConTypeRegister) {
             XGQBRegRestPwdTVCell *cell = [XGQBRegRestPwdTVCell cellWithType:XGQBRegResetPwdTVCellTypePayPwd];
+            _payPwdTextField = cell.textField;
             return cell;
         }else if (self.type ==XGQBRegResetPwdTVConTypeResetPayPwdWithID)
         {
             XGQBRegRestPwdTVCell *cell = [XGQBRegRestPwdTVCell cellWithType:XGQBRegResetPwdTVCellTypePayPwd];
+            _payPwdTextField = cell.textField;
             return cell;
         }else{
             return nil;
@@ -154,8 +284,6 @@
         return nil;
     }
 }
-
-
 
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
