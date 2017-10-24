e3d872bea0b1ff3a56af406a9ef3deedf19d0620
JIN Ke <zjujoke@qq.com>
2017-10-24 16:55:26 +0800

    银行卡交易记录修改和新增我的中奖纪录页面

--- src/page/MyLotteryRecordPage.js
+++ src/page/MyLotteryRecordPage.js
@@ -8,7 +8,8 @@ import {
 import BasePage from '../page/BasePage'
 import {colors} from '../constants/index'
 import Header from '../components/Header'
-
+import LotteryHeader from '../components/MyLotteryRecordHeader'
+import LotteryContent from '../components/MyLotteryRecordContent'
 
 class MyLotteryRecordPage extends BasePage {
 
@@ -16,42 +17,8 @@ class MyLotteryRecordPage extends BasePage {
         return (
             <View style={styles.container}>
                 <Header navigation={this.props.navigation} title='我的中奖纪录'/>
-                <View style = {styles.headerView}>
-                    <View style={styles.headerImg}>
-                        <Image source = {require('../res/img/LotteryRecord/wd_hongbao3.png')}>
-
-                            <Text style = {styles.redHeightTitle}>
-                                4756元
-                            </Text>
-                        </Image>
-
-                    </View>
-
-                    <View style = {styles.desLabel}>
-                        <Text style = {styles.desLabelText}>
-                            共参与
-                        </Text>
-                        <Text style = {styles.redHeightText}>
-                            78
-                        </Text>
-                        <Text style = {styles.desLabelText}>
-                            次活动,中奖
-                        </Text>
-                        <Text style = {styles.redHeightText}>
-                            67
-                        </Text>
-                        <Text style = {styles.desLabelText}>
-                            次.获得
-                        </Text>
-                        <Text style = {styles.redHeightText}>
-                            4756元
-                        </Text>
-                    </View>
-                        <Text style = {styles.detailLabel}>
-                            平台共发布134次活动,中奖池金额4567.89元
-                        </Text>
-
-                </View>
+                <LotteryHeader/>
+                <LotteryContent/>
             </View>
         );
     }
@@ -62,51 +29,7 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: colors.page_background,
     },
-    headerView:{
-        backgroundColor:'white',
-    },
-    headerImg:{
-        alignItems:'center',
-        marginTop:20,
-    },
-    redHeightTitle:{
-        color:'red',
-        fontSize: 35,
-        backgroundColor: 'transparent',
-        // alignItems:'center',
-        // justifyContent:'center',
-        marginTop:20,
-        marginLeft:70,
-        width:180,
-    },
-    desLabel:{
-        flexDirection:'row',
-        marginLeft:13,
-        marginTop:20,
-        height:30,
-        backgroundColor:'green',
-        alignItems:'center',
-    },
 
-    desLabelText:{
-        fontSize:14,
-        height:20,
-        backgroundColor:'yellow',
-        justifyContent:'center',
-        alignItems:'center',
-    },
-    redHeightText:{
-        color:'red',
-        fontSize:18,
-        height:20,
-        justifyContent:'center',
-        backgroundColor:'gray',
-    },
-    detailLabel:{
-        marginTop:10,
-        marginLeft:13,
-        marginBottom:18,
-    }
 
 });
 
