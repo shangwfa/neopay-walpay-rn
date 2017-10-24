abc63d322bd27de6c561447bb6b74d4c00833fdb
GITGL <644352260@qq.com>
2017-10-24 19:28:11 +0800

    @Version：1.0 @App：walpay-rn @Author：guolei @Describe: 1、API 统一存放所有的API地址 2、红包消息列表UI、部分逻辑 3、修改NetUtils 不同环境改变

---
+++ src/constants/API.js
@@ -0,0 +1,13 @@
+/**
+ * @author: carlos.guo
+ * @data:  2017/10/24.
+ * @description: 网络请求-url集合
+ */
+export let APIS = {
+    /**
+     * 网络请求-api
+     */
+
+    QUERY_RECENT_RED_PACKET_LIST: "packet/query_recent_red_packet_list",  //获取红包列表
+
+};
