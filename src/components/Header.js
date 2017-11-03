53be3ebc06c16a3cb42ed99a954c109a5c0aff94
jesus <Retina0.5K@Retina05Kdexiaobawang.local>
2017-11-03 17:00:51 +0800

    添加银行卡空页面

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,7 +20,6 @@ class Header extends Component {
         backgroundColor:colors.white,
         textColor:colors.black,
         isWhiteArrow:false,
-
     };
     static propTypes = {
         rightIcon: React.PropTypes.any,
