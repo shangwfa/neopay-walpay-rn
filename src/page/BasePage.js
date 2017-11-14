2c65ad4e20088e8d9e1be5b0c954153fba0a5700
shangwfa <shangwfa@gmail.com>
2017-11-14 10:38:35 +0800

    页面事件机制统一处理

--- src/page/BasePage.js
+++ src/page/BasePage.js
@@ -1,10 +1,5 @@
 import React, {Component} from 'react'
-import {
-    StyleSheet,
-    View,
-    Text,
-    Image,
-} from 'react-native'
+import {DeviceEventEmitter} from 'react-native'
 
 class BasePage extends Component {
     static navigationOptions = {
@@ -14,8 +9,18 @@ class BasePage extends Component {
         super(props)
         global.nav=this.props.navigation
         console.log("当前页面======"+nav.state.routeName)
-
     }
+
+    /*event={type:'xxxxx',data:{}*/
+    componentDidMount() {
+        DeviceEventEmitter.addListener(nav.state.routeName, (event) => {
+            this.emitEvent(event)
+        })
+    }
+    /**
+     * 1.emitEvent=(event)=>{console.log(event)}
+     * 2.DeviceEventEmitter.emit('activityList',{type:'xxx',{a:'xxxxx',b:'yyyyyy'}})
+     * */
 }
 
 
