9d00b24a4b0515157dd60e1e48dffe7a6cf801e4
GITGL <644352260@qq.com>
2017-11-16 18:32:31 +0800

    @Version：1.0 @App：walpay-rn @Author：guolei @Describe: 1、优化红包类型组件、选择银行卡支付组件 2、优化交易详情类型工具类

--- src/constants/API.js
+++ src/constants/API.js
@@ -9,6 +9,7 @@ export const APIS = {
      */
     QUERY_RED_PACKET_RECORD:"packet/query_red_packet_record_page",//红包交易明细
     QUERY_RECENT_RED_PACKET_LIST: "packet/query_recent_red_packet_list",  //获取红包列表
+    QUERY_RED_PACKET_MSG_PAGE: "message/query_red_packet_msg_page",  //获取红包消息分页
     GET_USER_INFO: 'user/get_user_info',//获取用户信息
     QUERY_USER_BILL_DETAIL: "pay/query_user_bill_detail",//查询用户账单详情
     GET_BANK_INFO_BY_CARD_NO: 'bank/get_bank_info_by_card_no',
